59ef890acb89fed9f246d8921ca479f2b3969436
Victoriia Kyselova <134308356+VictoriaKiselova@users.noreply.github.com>
2024-02-10 01:48:01 +0200

    add task 2-form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,33 @@
-console.log('form');
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', event => {
+  event.preventDefault();
+  let objValue = {
+    email: form.elements['email'].value,
+    message: form.elements['message'].value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(objValue));
+});
+
+function getValue(form) {
+  const localStorageValue = localStorage.getItem('feedback-form-state');
+  if (localStorageValue) {
+    const parsedObj = JSON.parse(localStorageValue);
+    form.elements['email'].value = parsedObj.email;
+    form.elements['message'].value = parsedObj.message;
+  }
+}
+getValue(form);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  let objValue = {
+    email: form.elements['email'].value,
+    message: form.elements['message'].value,
+  };
+  console.log(objValue);
+  localStorage.removeItem('feedback-form-state');
+  const input = document.querySelector('input');
+  input.value = '';
+  const textarea = document.querySelector('textarea');
+  textarea.value = '';
+});
